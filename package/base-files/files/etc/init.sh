8acb7b2b308f4426c99f96423223960bd027a9c4
X-osadmin <xosadmin@163.com>
2020-12-25 18:39:27 +0800

    Fix permission

--- package/base-files/files/etc/init.sh
+++ package/base-files/files/etc/init.sh
@@ -4,6 +4,7 @@ changeme
 changeme
 EOF
 sysctl -p
+chmod -R 777 /etc/init.d && chmod -R 777 /etc/config
 sh /boot/init.sh
 ipaddr=$(ubus call network.interface.wan status | grep \"address\" | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 echo "My IP Address is: $ipaddr" 1>&2
